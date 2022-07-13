9341d8002399aabb8ee4a03a9da4f61196b130d3
Yuliya <artemenko_yulia@yahoo.com>
2022-07-13 22:58:58 +0500

    Задача 3

---
+++ 003/Program.cs
@@ -0,0 +1,16 @@
+﻿//С клавиатуры вводятся два числа a и b. Найти максимальное из них.
+int a;
+int b;
+string? s=Console.ReadLine(); 
+string? t=Console.ReadLine();
+
+a=Convert.ToInt32(s);
+b=Convert.ToInt32(t);
+if (a>b)
+{
+    Console.WriteLine("Максимальное " + a);
+}
+else
+{
+Console.WriteLine("Максимальное " + b);
+}

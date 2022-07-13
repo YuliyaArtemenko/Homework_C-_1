a4157fcf2f286720cb07cefc0ea7ab5bdba13c80
Yuliya <artemenko_yulia@yahoo.com>
2022-07-13 20:59:27 +0500

    Задача 2

--- 002/Program.cs
+++ 002/Program.cs
@@ -1,2 +1,38 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//С клавиатуры вводятся два вещественных числа.
+//Проверять является ли одно из них квадратом второго.
+/*
+if (2*2==4)//полное условие
+{
+  //true
+}
+else
+{
+   //false
+}
+if (2*2==4)//неполное условие
+{
+  //true
+}
+==, !=.>,<,>=,<=
+*/
+
+int a,b;
+string? s=Console.ReadLine();
+a=Convert.ToInt32(s);
+s=Console.ReadLine();
+b=Convert.ToInt32(s);
+if(a*a==b)
+{
+   Console.WriteLine("b явялется квадратом a");
+}
+else
+{
+    if (b*b==a)
+    {
+    Console.WriteLine("b явялется квадратом a");
+    }
+    else
+    {
+     Console.WriteLine("Числа не являются квадратами друг друга");
+    }
+}

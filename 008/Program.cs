396ccc8316fc3be9d2d1ce794d79ae9d84f2fc17
Yuliya <artemenko_yulia@yahoo.com>
2022-07-14 00:08:58 +0500

    Задача 8

---
+++ 008/Program.cs
@@ -0,0 +1,12 @@
+﻿// Вывести на экран числа от -N до N
+int i=0;
+int a;
+string? s=Console.ReadLine(); 
+a=Convert.ToInt32(s);
+int b=-a;
+
+for (i=b; i<=a; i++)
+{
+     System.Console.Write($"{i} ");
+   
+}

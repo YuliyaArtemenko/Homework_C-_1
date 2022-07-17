cc7a21c7b559afa4344542ed9f2a208605f6e025
Yuliya <artemenko_yulia@yahoo.com>
2022-07-17 13:51:52 +0500

    Задача 013

--- 013/Program.cs
+++ 013/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Удалить вторую цифру целого числа введенного с клавиатуры.
+
+System.Console.WriteLine("Введите положительное число");
+int a=Convert.ToInt32(Console.ReadLine());
+int b=a/100;
+int c=a%10;
+int d=b%10;
+if(c>0)
+{
+   System.Console.WriteLine($"{d}{c}");
+}
+else
+{
+   System.Console.WriteLine(b);
+}
+
+
+
+
+
+

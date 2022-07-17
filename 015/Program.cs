b753ebc870013b70eff0e98e0046f1e53b9d434c
Yuliya <artemenko_yulia@yahoo.com>
2022-07-17 14:21:33 +0500

    Задача 015 не поняла как сделать

---
+++ 015/Program.cs
@@ -0,0 +1,25 @@
+﻿// С клавиатуры вводится целое число. Вывести третью цифру числа или
+//сообщить, что её нет.
+System.Console.WriteLine("Введите положительное число");
+int a=Convert.ToInt32(Console.ReadLine());
+int b=a%100;
+if(b<10)
+{
+    System.Console.WriteLine(b);
+}
+else
+{
+if(b>9)
+{
+   int d=b%10;
+   System.Console.WriteLine(d);
+}
+else
+{
+    if(b<100)
+    {
+      int c=b%10;
+      System.Console.WriteLine(c);
+    }
+}
+}

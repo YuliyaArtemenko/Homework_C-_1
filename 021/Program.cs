ebbb9fae61ef4d7f23b459590ebb4d2c915795bd
Yuliya <artemenko_yulia@yahoo.com>
2022-07-17 14:49:03 +0500

    Задача 021

---
+++ 021/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задать номер четверти, показать диапазоны для возможных координат
+System.Console.WriteLine("Введите номер четверти");
+int v=Convert.ToInt32(Console.ReadLine());
+if (v==1)
+{
+    System.Console.WriteLine("Координаты X [0;+▲], Координата Y [0;+▲]");
+}
+else
+{
+ if (v==2)
+ {
+ System.Console.WriteLine("Координаты X [0;-▲], Координата Y [0;+▲]");
+ }
+ else 
+ {
+    if (v==3)
+    {
+        System.Console.WriteLine("Координаты X [0;-▲], Координата Y [0;-▲]");
+    }
+    else
+        {
+        System.Console.WriteLine("Координаты X [0;+▲], Координата Y [0;-▲]");
+        }
+ }
+ 
+}

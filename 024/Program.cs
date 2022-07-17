3408ae98ac1aa2774e092d16b88e838937bdf0dd
Yuliya <artemenko_yulia@yahoo.com>
2022-07-17 15:43:30 +0500

    Задача 024

---
+++ 024/Program.cs
@@ -0,0 +1,9 @@
+﻿// Показать таблицу квадратов чисел от 1 до N
+System.Console.WriteLine("Введите число N");
+int N=Convert.ToInt32(Console.ReadLine());
+int i;
+for (i=1; i<=N; i++)
+{
+    double s=Math.Pow(i,2);
+    System.Console.Write($"{s} ");
+}

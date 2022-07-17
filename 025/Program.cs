857236374bf056f64f2249e9e3e78ff3a5c88630
Yuliya <artemenko_yulia@yahoo.com>
2022-07-17 15:45:43 +0500

    Задача 025

---
+++ 025/Program.cs
@@ -0,0 +1,9 @@
+﻿// Найти кубы чисел от 1 до N
+System.Console.WriteLine("Введите число N");
+int N=Convert.ToInt32(Console.ReadLine());
+int i;
+for (i=1; i<=N; i++)
+{
+    double s=Math.Pow(i,3);
+    System.Console.Write($"{s} ");
+}

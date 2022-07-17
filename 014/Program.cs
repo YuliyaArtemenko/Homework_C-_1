653ee5dc762570eee91bad054c6aa48d1638a442
Yuliya <artemenko_yulia@yahoo.com>
2022-07-17 13:59:46 +0500

    Задча 014

---
+++ 014/Program.cs
@@ -0,0 +1,15 @@
+﻿// SС клавиатуры вводятся два числа a и b. Выяснить, кратно ли число a
+// числу b, если нет, вывести остаток от деления a на b.
+System.Console.WriteLine("Введите положительное число a");
+int a=Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите положительное число b");
+int b=Convert.ToInt32(Console.ReadLine());
+if (a%b==0)
+{
+    System.Console.WriteLine("Число а кратно b");
+}
+else
+{
+    int c=a%b;
+    System.Console.WriteLine(c);
+}

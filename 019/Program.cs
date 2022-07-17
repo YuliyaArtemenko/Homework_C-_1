ff94a900b7870ecd8f8dae5b7e9d5e56357ee538
Yuliya <artemenko_yulia@yahoo.com>
2022-07-17 12:34:03 +0500

    Задача 019

---
+++ 019/Program.cs
@@ -0,0 +1,10 @@
+﻿// Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
+//!(X || Y)==!X && !Y
+
+bool x,y,f1,f2;
+x=true;
+y=false;
+f1=!(x || y);
+f2=!x && !y;
+System.Console.WriteLine($"{x} {y} {f1} {f2}");
+
---
+++ Loop/Program.cs
@@ -0,0 +1,13 @@
+﻿// 
+int N=10;
+int i -0;
+while(i<N)//пока
+{
+    i=i+1;
+    System.Console.WriteLine($"{i} ");
+}
+//for
+for (i=0, i<N, int=i+1)
+{
+    System.Console.WriteLine($"{i} ");
+}

d6e72c92ed4d46c2fb65cd9ec354bc923332fc69
Yuliya <artemenko_yulia@yahoo.com>
2022-07-17 16:01:45 +0500

    Задача 030

---
+++ 030/Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу вычисления произведения чисел от 1 до N
+int CountDigits(int N)
+{
+    int i;
+    int k=1;
+    for (i=1; i<=N; i++)
+    {
+        k=k*i;
+    }
+    return k;
+}
+System.Console.WriteLine(CountDigits(5));

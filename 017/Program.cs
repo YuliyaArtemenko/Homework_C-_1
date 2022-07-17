6c8b70befed7e7d3cefdf950b0d120efc19a8c64
Yuliya <artemenko_yulia@yahoo.com>
2022-07-17 14:24:48 +0500

    Задача 017

---
+++ 017/Program.cs
@@ -0,0 +1,11 @@
+﻿// Дано число обозначающее день недели. Выяснить является номер дня
+//недели выходным
+System.Console.WriteLine("Введите число дня недели");
+int a=Convert.ToInt32(Console.ReadLine());
+if (a>5)
+{
+    System.Console.WriteLine("Выходной день");
+}
+else{
+    System.Console.WriteLine("Будний день");
+}

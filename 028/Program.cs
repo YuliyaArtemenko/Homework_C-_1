9035ea153d182eb0dc515e6bdc7f718457e5a031
Yuliya <artemenko_yulia@yahoo.com>
2022-07-17 13:21:06 +0500

    Задача 028

--- 028/Program.cs
+++ 028/Program.cs
@@ -1,2 +1,25 @@
 ﻿// Определить количество цифр в числе. Сделать подпрограмму.
 
+/*
+int N=1264;
+int k=0;
+while(N!=0)
+{
+    k++;
+    N=N/10;
+}
+System.Console.WriteLine(k);
+*/
+
+int CountDigits(int N)
+{
+    if (N==0) return 1;
+    int k=0;
+    while(N!=0)
+    {
+        k++;
+        N=N/10;
+    }
+    return k;
+}
+System.Console.WriteLine(CountDigits(125));

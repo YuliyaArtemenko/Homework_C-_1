be87f7570de222020bb5b26467ffcd696f4672bb
Yuliya <artemenko_yulia@yahoo.com>
2022-07-17 15:51:28 +0500

    Задача 026

---
+++ 026/Program.cs
@@ -0,0 +1,10 @@
+﻿// Найти сумму чисел от 1 до А
+System.Console.WriteLine("Введите число A");
+int A=Convert.ToInt32(Console.ReadLine());
+int i;
+int sum=0;
+for (i=1; i<=A; i++)
+{
+   sum=sum+i;
+}
+ System.Console.WriteLine(sum );

0ab71fc325eaada904546393f3b9d51d17094f3b
Yuliya <artemenko_yulia@yahoo.com>
2022-07-17 13:33:09 +0500

    Задача 012

--- 012/Program.cs
+++ 012/Program.cs
@@ -1,1 +1,15 @@
-﻿// 
+﻿// С клавиатуры вводится целое число из  диапазона [10, 99].
+// Показать наибольшую цифру числа
+int a=56;
+int b=a/10;
+int c=a%10;
+if (b>c)
+{ 
+    System.Console.WriteLine(b);
+}
+else 
+{
+    System.Console.WriteLine(c);
+}
+
+

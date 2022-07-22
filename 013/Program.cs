a2ee78ee73f38cba747eaa326b85fb7f01adaa0f
Yuliya <artemenko_yulia@yahoo.com>
2022-07-23 00:35:30 +0500

    Задача013 пояснение

--- 013/Program.cs
+++ 013/Program.cs
@@ -1,6 +1,6 @@
 ﻿// Удалить вторую цифру целого числа введенного с клавиатуры.
 
-System.Console.WriteLine("Введите положительное число");
+/*System.Console.WriteLine("Введите положительное число");
 int a=Convert.ToInt32(Console.ReadLine());
 int b=a/100;
 int c=a%10;
@@ -13,6 +13,14 @@ else
 {
    System.Console.WriteLine(b);
 }
+*/
+int n=Convert.ToInt32(Console.ReadLine());
+int d0=n%10;
+int d1=n/10%10;
+int d2=n/100;
+int n1=d2*10+d0;
+System.Console.WriteLine($"{d0}{d1}{d2}");
+System.Console.WriteLine($"{n1}");
 
 
 

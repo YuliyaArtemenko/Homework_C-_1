1bae1bf730a1dd991648f4725b215bcc14e744b6
Yuliya <artemenko_yulia@yahoo.com>
2022-07-23 01:18:54 +0500

    Задача 004 массивом

--- 004/Program.cs
+++ 004/Program.cs
@@ -1,4 +1,12 @@
 ﻿// 4. По заданному с клавиатуры номеру дня недели вывести его название
+//массивом
+Console.Write("Введите номер недели: ");
+string userD=Console.ReadLine();
+int d=Convert.ToInt32(userD);
+string[] weekDays = new string[] {"Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"};
+System.Console.WriteLine(weekDays[d-1]);
+/*
+a=Convert.ToInt32(s);
 int a;
 string? s=Console.ReadLine(); 
 a=Convert.ToInt32(s);
@@ -19,6 +27,7 @@ switch(a)
     
 
 }
+*/
 //или int dayOfWeek=Convert.ToInt(Console.ReadLine()); camelCase
 //int DayOfWeek-PascalCase
 /*if(a==1)

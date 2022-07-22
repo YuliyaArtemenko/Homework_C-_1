b1fe9a9e15b32ff702ac7306da7ec305c50f073c
Yuliya <artemenko_yulia@yahoo.com>
2022-07-23 00:51:01 +0500

    Задача 013_пояснение

--- 013/Program.cs
+++ 013/Program.cs
@@ -14,6 +14,7 @@ else
    System.Console.WriteLine(b);
 }
 */
+/*
 int n=Convert.ToInt32(Console.ReadLine());
 int d0=n%10;
 int d1=n/10%10;
@@ -21,6 +22,28 @@ int d2=n/100;
 int n1=d2*10+d0;
 System.Console.WriteLine($"{d0}{d1}{d2}");
 System.Console.WriteLine($"{n1}");
+*/
+//Вариант 2 - Удаление с начала
+
+int n=Convert.ToInt32(Console.ReadLine());
+int k=CountDigits(n);
+int n2=n%(int)Math.Pow(10,k-2);//берем в типы int вместо double
+int n3=n/(int)Math.Pow(10,k-1);
+
+System.Console.WriteLine($"{n3}{n2}");
+
+int CountDigits(int N)
+{
+    if (N==0) return 1;
+    int k=0;
+    while(N!=0)
+    {
+        k++;
+        N=N/10;
+    }
+    return k;
+}
+//System.Console.WriteLine(CountDigits(n));
 
 
 

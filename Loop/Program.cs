981e30ae0dedd574854d8f9072ef53d9b55f7dcb
Yuliya <artemenko_yulia@yahoo.com>
2022-08-11 22:53:22 +0500

    Loop

--- Loop/Program.cs
+++ Loop/Program.cs
@@ -1,13 +1,21 @@
 ﻿// 
 int N=10;
-int i -0;
-while(i<N)//пока
+int i=0;
+while(i<N)//пока - универсальный цикл
 {
     i=i+1;
     System.Console.WriteLine($"{i} ");
 }
 //for
-for (i=0, i<N, int=i+1)
+for (i=0, i<N, i=i+1)
 {
     System.Console.WriteLine($"{i} ");
 }
+//do
+int a;
+do 
+{
+   System.Console.WriteLine("Введите положительное число");
+   int a=Convert.ToInt32(Console.ReadLine());
+}
+while(a<=0);
---
+++ Variables/Program.cs
@@ -0,0 +1,10 @@
+﻿// /NET
+//значимые типы (value types)
+int a;
+doubleb;
+//ссылочные типы (reference types)
+string  s;
+s="Hello";
+int[] arr;
+arr=new int[5];
+//переменные храняться в стеке (очередь)

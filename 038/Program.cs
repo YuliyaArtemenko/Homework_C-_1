a8e4ce072971cf87c2e13e8246e085b6e631f380
Yuliya <artemenko_yulia@yahoo.com>
2022-08-12 16:49:46 +0500

    Задача 38

---
+++ 038/Program.cs
@@ -0,0 +1,18 @@
+﻿// 038 В одномерном массиве из 123 чисел 
+//найти количество элементов из отрезка [10,99]
+int N=123;
+int[] a=new int[N];
+Random random=new Random();
+for(int i=0; i<a.Length; i++)
+    a[i]=random.Next(0,124);
+
+
+//вывод массива (до обработки)
+for (int i=0;i<a.Length; i++)
+System.Console.Write($"{a[i],4}  ");
+
+//решение задачи
+int count=0;
+for (int i=10;i<100; i++)
+     count=count+1;   
+System.Console.WriteLine(count);

fdbb5c40f1e48dcf261539925fa9228f31ded264
Yuliya <artemenko_yulia@yahoo.com>
2022-08-12 14:21:50 +0500

    Пояснения

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -1,10 +1,19 @@
 ﻿// /NET
 //значимые типы (value types)
 int a;
-doubleb;
+double  b;
+a=4;
 //ссылочные типы (reference types)
 string  s;
 s="Hello";
+string s1;
+s1=s;//копирует
 int[] arr;
 arr=new int[5];
 //переменные храняться в стеке (очередь)
+arr[0]=1;
+int[] arr2;
+arr2=arr;//массив ссылается на тот же массив, при изменении нвого старый тоже еняется
+arr2[0]=2;
+
+System.Console.WriteLine();

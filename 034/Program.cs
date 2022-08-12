1c37c8d782d058b7a3fcdb39628dcf51f3215dd2
Yuliya <artemenko_yulia@yahoo.com>
2022-08-12 15:51:55 +0500

    Задача 034 полностью

--- 034/Program.cs
+++ 034/Program.cs
@@ -1,15 +1,27 @@
 ﻿// 034 Задать массив из 12 элементов, заполненных числами из [-9, 9]. 
 //Найти сумму положительных/отрицательных элементов массива
 
-int N=12;
+int N=12;//инициализация массива
 int[] a=new int[N];
 Random random=new Random();//если в скобках указать число, то набор случайных числе бует одинаков
 for(int i=0; i<a.Length; i++)
     a[i]=random.Next(-9,10);
-    int sumPositive=0;//или sumNegative
+
+//вывод массива (до обработки)
+for (int i=0;i<a.Length; i++)
+System.Console.Write($"{a[i],4}  ");//цифра 4 означает количество пустых пробелом между элементами массива    
+
+//решение задачи
+int sumPositive=0;//или sumNegative, если считаем отрицательные
 for (int i=0;i<a.Length; i++)
 if(a[i]>0)
-    sumPositive=sumPositive+a[i];//или += a[i]
-System.Console.WriteLine(sumPositive);
+    sumPositive=sumPositive+a[i];//или += a[i] - увеличить сумму на a[i]
+
+
+int sumNegative=0;
 for (int i=0;i<a.Length; i++)
-System.Console.Write($"{a[i],4}");//цифра 4 означает количество пустых пробелом между элементами массива
+   if(a[i]<0)
+      sumNegative+=a[i];
+
+System.Console.WriteLine(sumPositive);
+System.Console.WriteLine(sumNegative);
